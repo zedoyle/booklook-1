461dbb84fcb7015990dced7788cafccc35ede3f0
Zach Doyle <zedoyle@gmail.com>
2015-04-23 17:09:33 -0600

    parameters pass

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var app = express();
+var bodyparser = require('body-parser');
+var pshell = require('python-shell');
+var filesystem = require('fs');
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.use(express.static(__dirname + '/views'));
+
+app.use(bodyparser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(__dirname + '/public/'));
 
@@ -9,6 +17,36 @@ app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'
 
 app.get('/', function(req, res) {
 	res.render('index.html');
+});
+
+app.post('/sendquery', function(req, res) {
+	var fname='placeholder.json';
+	var data="";
+	var the_query = req.body.query;
+	var jsonfile = null;
+	//pshell.run('placeholder.py ', function (err) {
+	//	if (err) throw err;
+	//});
+	filesystem.stat(fname, function(error, stats){
+		filesystem.open(fname,"r",function (error, fd){
+			var buffer = new Buffer(stats.size);
+			filesystem.read(fd, buffer, 0, buffer.length, null,
+				function(error, bytesRead, buffer){
+				//data = buffer.toJSON();
+				//console.log("DATA:"+JSON.stringify(data));
+				var filestringed = buffer.toString();
+				console.log("BUFFER:"+filestringed);
+				jsonfile = JSON.parse(filestringed);
+				console.log(jsonfile);
+				//console.log(buffer)
+			});
+			//console.log(error)
+		});
+		//console.log(error)
+	});
+	app.render('results',{jsonobj: jsonfile},function(err, html){
+		res.send(html);
+	});
 });
 
 app.set('port', (process.env.PORT || 3000));
